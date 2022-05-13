f7a3c40a733697151ef6cab395bc6012a6e64bc1
Joshua Mayanja <joshua@drexsoft.org>
2022-05-13 14:50:35 +0000

    Check Everything

--- entry_point.sh
+++ entry_point.sh
@@ -2,4 +2,4 @@
 
 RUN_PORT=${PORT:-8989}
 
-/app/.env/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${RUN_PORT}"
+/app/env/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${RUN_PORT}"
