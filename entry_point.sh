43eb2a7a1920de35fe8e3f48e6778ac6adddbd8c
Joshua Mayanja <joshua@drexsoft.org>
2022-05-15 03:35:13 +0000

    Fixed Entry Point

--- entry_point.sh
+++ entry_point.sh
@@ -2,4 +2,4 @@
 echo "Running DrexSpamAPI"
 RUN_PORT=${PORT:-8989}
 
-/app/env/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${RUN_PORT}"
+/app/env/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${RUN_PORT}" app.main:app
